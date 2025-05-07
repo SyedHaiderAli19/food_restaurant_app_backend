0af0eeeafeb88cafe4e5deb6c46c477b784d6f28
SyedHaider <syedhaider421@gmail.com>
2025-05-07 17:26:02 +0500

    SignUp Controller

--- src/auth/entrypoint/AuthController.ts
+++ src/auth/entrypoint/AuthController.ts
@@ -1,14 +1,17 @@
 import ITokenService from "../services/ITokenService";
 import SignInUseCase from "../usecases/SignInUseCase";
 import * as express from 'express'
+import SignUpUseCase from "../usecases/SignUpUseCase";
 
     export default  class AuthController {
 
         private readonly signInUseCase: SignInUseCase
+        private readonly signUpUseCase: SignUpUseCase
         private readonly tokenService: ITokenService
 
-        constructor(signInUseCase: SignInUseCase, tokenService: ITokenService){
+        constructor(signInUseCase: SignInUseCase, signUpUseCase: SignUpUseCase, tokenService: ITokenService){
             this.signInUseCase = signInUseCase
+            this.signUpUseCase = signUpUseCase
             this.tokenService = tokenService
         }
 
@@ -17,6 +20,33 @@ import * as express from 'express'
                 const {email,password}= req.body
                 console.log(email,password)
                 return this.signInUseCase.execute(email,password)
+                .then((id: string) => {
+                    console.log("User id in controller: ", id);
+                    const jwtToken =  this.tokenService.encode(id);
+                    console.log("Auth token: ", jwtToken)
+                    res.status(200).json({auth_token: jwtToken})
+                })
+                .catch((err: Error)=> {
+                    console.log("Error in authController: ", err);
+                    res.status(404).json({error:err.message})
+                })
+
+            }
+            catch(e){
+                return res.status(400).json({error:e})
+
+            }
+
+        }
+
+
+        //signup controller
+
+        public async signUp(req:express.Request, res: express.Response): Promise<any>{
+            try{
+                const {name,email,password,authType}= req.body
+                console.log(email,password)
+                return this.signUpUseCase.execute(name,password,authType,email)
                 .then((id: string) => {
                     console.log("User id in controller: ", id);
                     const jwtToken =  this.tokenService.encode(id);
