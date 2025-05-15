0f7f17127a35109829cd99ac9d49bda8a12e45f8
SyedHaider <syedhaider421@gmail.com>
2025-05-15 16:10:44 +0500

    CORS Error Fix

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import Constants from '../constants'
 import CompositionRoot from './CompositionRoot'
+const cors = require('cors');
 
 
 dotenv.config()
@@ -12,7 +13,7 @@ const PORT = process.env.PORT
 const constants = new Constants()
 
 const app = express()
-
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/auth',CompositionRoot.authRouter())
